d6f014e7eed681b4f68ad9f124125e11a27c64bf
wenyu <17601632475@163.com>
2017-09-05 14:46:00 +0800

    metronic

---
+++ me/metronic/assets/crm/pages/scripts/table-advanced.js
@@ -0,0 +1,106 @@
+var TableAdvanced = function () {
+
+
+    var initTable4 = function () {
+        var table = $('#sample_4');
+
+        /* Formatting function for row expanded details */
+        function fnFormatDetails(oTable, nTr) {
+            var aData = oTable.fnGetData(nTr);
+            var sOut = '<table>';
+            sOut += '<tr><td>Platform(s):</td><td>' + aData[2] + '</td></tr>';
+            sOut += '<tr><td>Engine version:</td><td>' + aData[3] + '</td></tr>';
+            sOut += '<tr><td>CSS grade:</td><td>' + aData[4] + '</td></tr>';
+            sOut += '<tr><td>Others:</td><td>Could provide a link here</td></tr>';
+            sOut += '</table>';
+
+            return sOut;
+        }
+
+        /*
+         * Insert a 'details' column to the table
+         */
+
+
+
+
+        var oTable = table.dataTable({
+
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "No data available in table",
+                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
+                "infoEmpty": "No entries found",
+                "infoFiltered": "(filtered1 from _MAX_ total entries)",
+                "lengthMenu": "Show _MENU_ entries",
+                "search": "Search:",
+                "zeroRecords": "No matching records found"
+            },
+
+            "columnDefs": [{
+                "orderable": false,
+                "targets": [0]
+            }],
+            "order": [
+                [1, 'asc']
+            ],
+            "lengthMenu": [
+                [5, 15, 20, -1],
+                [5, 15, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 10,
+        });
+
+        var tableWrapper = $('#sample_4_wrapper'); // datatable creates the table wrapper by adding with id {your_table_jd}_wrapper
+        var tableColumnToggler = $('#sample_4_column_toggler');
+
+        /* modify datatable control inputs */
+        tableWrapper.find('.dataTables_length select').select2(); // initialize select2 dropdown
+
+        /* Add event listener for opening and closing details
+         * Note that the indicator for showing which row is open is not controlled by DataTables,
+         * rather it is done here
+         */
+        table.on('click', ' tbody td .row-details', function () {
+            var nTr = $(this).parents('tr')[0];
+            if (oTable.fnIsOpen(nTr)) {
+                /* This row is already open - close it */
+                $(this).addClass("row-details-close").removeClass("row-details-open");
+                oTable.fnClose(nTr);
+            } else {
+                /* Open this row */
+                $(this).addClass("row-details-open").removeClass("row-details-close");
+                oTable.fnOpen(nTr, fnFormatDetails(oTable, nTr), 'details');
+            }
+        });
+
+        /* handle show/hide columns*/
+        $('input[type="checkbox"]', tableColumnToggler).change(function () {
+            /* Get the DataTables object again - this is not a recreation, just a get of the object */
+            var iCol = parseInt($(this).attr("data-column"));
+            var bVis = oTable.fnSettings().aoColumns[iCol].bVisible;
+            oTable.fnSetColumnVis(iCol, (bVis ? false : true));
+        });
+    }
+
+
+
+    return {
+
+        //main function to initiate the module
+        init: function () {
+
+            if (!jQuery().dataTable) {
+                return;
+            }
+            initTable4();
+        }
+
+    };
+
+}();
